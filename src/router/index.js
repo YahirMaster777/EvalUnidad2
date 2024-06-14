a266bc0d2e2ed795eee65fbb346c1b2f7c7ffa1e
Yair Tolentino <92339589+YahirMaster777@users.noreply.github.com>
2024-06-13 20:59:51 -0600

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,63 @@
+import { createRouter, createWebHistory } from 'vue-router'
+//import HomeView from '../views/HomeView.vue'
+import LoginView from '../components/login.vue'
+import RegisterView from '../components/registerUser.vue'
+import SideBar from '../components/sidebar.vue'
+import Personas from '../components/personas.vue'
+// import Usuarios from '../components/usuarios.vue'
+import Nacimientos from '../components/nacimientos.vue'
+import Seguimiento from '../components/Seguimiento.vue'
+import ListaVacunas from '../components/listaVacunas.vue'
+import Vacunas from '../components/vacunas.vue'
+import Medicos from '../components/datosMedicos.vue'
+import Infante from '../components/datosPaciente.vue'
+import Contacto from '../components/datosContacto.vue'
+import Footer from '../components/footerView.vue'
+
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: LoginView
+     
+    },
+    {
+      path: '/login',
+      name: 'login',
+      
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      
+      component: RegisterView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: SideBar,
+      children:[
+        {path:'/personas', name:'personas', component:Personas}
+        ,{path:'/nacimientos',name:'nacimientos', component:Nacimientos}
+        ,{path:'/seguimiento',name:'seguimiento', component:Seguimiento}
+        ,{path:'/vacunas',name:'vacunas', component:Vacunas}
+        ,{path:'/listaVacunas',name:'listaVacunas', component:ListaVacunas}
+        ,{path:'/datosContacto',name:'contacto', component: Contacto}
+        ,{path:'/datosPaciente',name:'infante', component: Infante}
+        ,{path:'/datosMedicos',name:'Medicos', component: Medicos},
+        {path:'/footer', name:'Footer',component:Footer}
+      
+      
+      ]
+      
+    }
+  ]
+})
+
+export default router
